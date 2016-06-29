57bbfa0c6a10b0c1655635b0435e3d6d95079589
Alexey Bokov <bokov-aleksej@yandex.ru>
2016-06-29 17:25:54 +0300

    add saving to blob

--- Microsoft.WindowsAzure.Storage/samples/Genomic-Storage/genomic-storage.cpp
+++ Microsoft.WindowsAzure.Storage/samples/Genomic-Storage/genomic-storage.cpp
@@ -4,6 +4,9 @@
 
 #include "genomic-storage.h"
 
+#include "cpprest/filestream.h"
+#include "cpprest/containerstream.h"
+
 using namespace azure;
 using namespace storage;
 
@@ -19,28 +22,131 @@ GenomicStorage::~GenomicStorage() {
 // clean up after all
 };
 
-bool GenomicStorage::Init(const std::string &account_name,const std::string &account_key) {
-	if ( (account_name.length()==0) || (account_key.length()==0)) {
-		error_descr = "Wrong length of account_name or account_key, account_name=\""+account_name+"\", account_key=\""+account_key+"\"\n";
+bool GenomicStorage::Init(AzureStorageParams &param) {
+try {
+	if (param.Validate() == false) {
+		error_descr = "Wrong length of one of params : "+std::string(param.GetDescription())+"\n";
 		return false;
 	}
 #ifdef __DEBUG_GENOMIC__
-        std::cout<<"GenomicStorage::Init(account_name=\""<<account_name<<"\", account_key=\""<<account_key<<"\")\n";
+        std::cout<<"GenomicStorage::Init("<<param.GetDescription()<<")\n";
 #endif
-	storage_connection_string="DefaultEndpointsProtocol=https;AccountName="+account_name+";AccountKey="+account_key;
+	storage_connection_string="DefaultEndpointsProtocol=https;AccountName="+param.account_name+";AccountKey="+param.account_key;
+
+	storage_account = azure::storage::cloud_storage_account::parse(storage_connection_string);
+	blob_client = storage_account.create_cloud_blob_client();
+	blob_container = blob_client.get_container_reference(param.container_name);
+	
+
 #ifdef __DEBUG_GENOMIC__
 	std::cout<<"storage_connection_string = \""<<storage_connection_string<<"\"\n";
 #endif
-	return true;
+
+
+} catch (const std::invalid_argument &ex )  {
+	error_descr = "Exception std::invalid_argument caught : " + std::string(ex.what());
+#ifdef __DEBUG_GENOMIC__
+	std::cout<<error_descr<<"\n";
+#endif
+	return false;
+} catch  (const std::exception &ex )  {
+        error_descr = "Exception std::exception caught : " + std::string(ex.what());
+#ifdef __DEBUG_GENOMIC__
+        std::cout<<error_descr<<"\n";
+#endif
+	return false;
+} catch (...) {
+        error_descr = "Unknown exception caught ";
+#ifdef __DEBUG_GENOMIC__
+        std::cout<<error_descr<<"\n";
+#endif
+	return false;
+} 
+
+return true;
 };
 
-bool GenomicStorage::AddBlob(void) {
-	return true;
+
+bool GenomicStorage::SaveFileToBlob(const char *blob_name, const char *file_name, bool is_public) {
+try {
+#ifdef __DEBUG_GENOMIC__
+        std::cout<<"GenomicStorage::SaveFileToBlob(blob_name=\""<<blob_name<<"\"file_name=\""<<file_name<<"\",is_public="<<(int)is_public<<")\n";
+#endif
+
+
+        blob_container.create_if_not_exists();
+
+	//setting up permissions 
+	azure::storage::blob_container_permissions permissions;
+	if (is_public)
+		permissions.set_public_access(azure::storage::blob_container_public_access_type::blob);
+	else
+		permissions.set_public_access(azure::storage::blob_container_public_access_type::off);
+	blob_container.upload_permissions(permissions);
+
+	
+	concurrency::streams::istream input_stream = concurrency::streams::file_stream<uint8_t>::open_istream(file_name).get();
+
+
+	azure::storage::cloud_block_blob blob1 = blob_container.get_block_blob_reference(blob_name);
+#ifdef __DEBUG_GENOMIC__
+        std::cout<<"blob_uri="<<blob1.uri().primary_uri().to_string()<<"\n";
+#endif
+
+	blob1.upload_from_stream(input_stream);
+	input_stream.close().wait();
+
+
+
+} catch (const std::invalid_argument &ex )  {
+        error_descr = "Exception std::invalid_argument caught : " + std::string(ex.what());
+#ifdef __DEBUG_GENOMIC__
+        std::cout<<error_descr<<"\n";
+#endif
+        return false;
+} catch  (const std::exception &ex )  {
+        error_descr = "Exception std::exception caught : " + std::string(ex.what());
+#ifdef __DEBUG_GENOMIC__
+        std::cout<<error_descr<<"\n";
+#endif
+        return false;
+} catch (...) {
+        error_descr = "Unknown exception caught ";
+#ifdef __DEBUG_GENOMIC__
+        std::cout<<error_descr<<"\n";
+#endif
+        return false;
+} 
+return true;
 };
 
-bool GenomicStorage::ReadBlob(void) {
-	return true;
+bool GenomicStorage::SaveBlobToFile(const char *blob_name, const char *file_name) {
+try {
+	return false;
+
+} catch (const std::invalid_argument &ex )  {
+        error_descr = "Exception std::invalid_argument caught : " + std::string(ex.what());
+#ifdef __DEBUG_GENOMIC__
+        std::cout<<error_descr<<"\n";
+#endif
+        return false;
+} catch  (const std::exception &ex )  {
+        error_descr = "Exception std::exception caught : " + std::string(ex.what());
+#ifdef __DEBUG_GENOMIC__
+        std::cout<<error_descr<<"\n";
+#endif
+        return false;
+} catch (...) {
+        error_descr = "Unknown exception caught ";
+#ifdef __DEBUG_GENOMIC__
+        std::cout<<error_descr<<"\n";
+#endif
+        return false;
+} 
+return true;
 };
+
+
 
 
 } // namespace azure::storage::samples
--- Microsoft.WindowsAzure.Storage/samples/Genomic-Storage/genomic-storage.h
+++ Microsoft.WindowsAzure.Storage/samples/Genomic-Storage/genomic-storage.h
@@ -12,17 +12,52 @@
 
 namespace genomic {
 
+class AzureStorageParams {
+	std::string descr;
+public:
+	std::string account_name,account_key,container_name,blob_name,upload_file;
+	const char * GetDescription(void) {
+                descr = "account_name=\"" + account_name + "\"";
+                descr += ",account_key=\"" + account_key + "\"";
+		descr += ",container_name=\"" + container_name + "\"";
+                descr += ",blob_name=\"" + blob_name + "\"";
+                descr += ",upload_file=\"" + upload_file + "\"";
+
+		return descr.c_str();
+	};
+	// check params for correct lentgh
+	bool Validate(void) const {
+		if (account_name.length()==0)
+			return false;
+		if (account_key.length()!=64)
+			return false;
+		if (container_name.length()==0)
+			return false;
+		if (blob_name.length()==0)
+			return false;
+		if (upload_file.length()==0)
+			return false;
+		return true;
+	};
+};
+
 class GenomicStorage {
 
 	utility::string_t storage_connection_string;
 	std::string error_descr;
 
+//     Azure storage objects
+	azure::storage::cloud_storage_account storage_account;
+	azure::storage::cloud_blob_client blob_client;
+	azure::storage::cloud_blob_container blob_container;
+
 public:
 	GenomicStorage();
 	virtual ~GenomicStorage();
-	bool Init(const std::string & account_name,const std::string & account_key);
-	bool AddBlob(void);
-	bool ReadBlob(void);
+	bool Init(AzureStorageParams &azure_params);
+
+	bool SaveFileToBlob(const char *blob_name, const char *file_name, bool is_public);
+	bool SaveBlobToFile(const char *blob_name, const char *file_name);
 
 	const char * GetErrorDescription(void) { 
 		return error_descr.c_str();
