13877be2b53e49b42ddea961b779b2aca904c34a
Alexey Bokov <bokov-aleksej@yandex.ru>
2016-06-29 11:32:19 +0300

    make it buildable

--- Microsoft.WindowsAzure.Storage/samples/Genomic-Storage/genomic-storage.cpp
+++ Microsoft.WindowsAzure.Storage/samples/Genomic-Storage/genomic-storage.cpp
@@ -9,6 +9,8 @@ using namespace storage;
 
 namespace genomic {
 
+#define __DEBUG_GENOMIC__  1
+
 GenomicStorage::GenomicStorage() {
 // do nothing, RFU actually
 };
@@ -17,17 +19,17 @@ GenomicStorage::~GenomicStorage() {
 // clean up after all
 };
 
-bool GenomicStorage::Init(const std::string &account_name,const std::string & account_key) {
-	if (account_name.length()==0) || (account_key.lenght()==0) {
-		error_descr = "Wrong length of account_name or account_key, account_name=\""<<account_name<<"\", account_key=\""<<account_key<<"\"\n";
+bool GenomicStorage::Init(const std::string &account_name,const std::string &account_key) {
+	if ( (account_name.length()==0) || (account_key.length()==0)) {
+		error_descr = "Wrong length of account_name or account_key, account_name=\""+account_name+"\", account_key=\""+account_key+"\"\n";
 		return false;
 	}
 #ifdef __DEBUG_GENOMIC__
         std::cout<<"GenomicStorage::Init(account_name=\""<<account_name<<"\", account_key=\""<<account_key<<"\")\n";
 #endif
-	storage_connection_string="DefaultEndpointsProtocol=https;AccountName="+account_name+";AccountKey="+myaccountkey;
-#ifdef _DEBUG_GENOMIC__
-	std::cout<<"storage_connection_string = \"<<storage_connection_string<<\"\n";
+	storage_connection_string="DefaultEndpointsProtocol=https;AccountName="+account_name+";AccountKey="+account_key;
+#ifdef __DEBUG_GENOMIC__
+	std::cout<<"storage_connection_string = \""<<storage_connection_string<<"\"\n";
 #endif
 	return true;
 };
